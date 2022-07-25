f1fc095dc4be4b14873d48ada518b3d472a8345e
DeimanteRag <d.ragauskaitee@gmail.com>
2022-07-25 13:19:05 +0300

    My git pages

---
+++ src/Api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a590b7843fmshdd25804baa28105p102ae5jsnfcdc8d7bbd68",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "aee83dcfe869cca9c119328975fc3b4b";
